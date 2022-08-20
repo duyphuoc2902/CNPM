1b204a27e1c42151b3995ffd002acf3c5b19471d
duyphuoc2902 <duyphuoc292@gmail.com>
2022-08-20 22:18:00 +0700

    add new project

---
+++ WindowsFormsApp1/DAO/AccountDAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsFormsApp1.DAO
+{
+    internal class AccountDAO
+    {
+        private static AccountDAO instance;
+        private string Username, Password;
+        public static AccountDAO Instance
+        {
+            get { if (instance == null) instance = new AccountDAO(); return AccountDAO.instance; }
+            private set { AccountDAO.instance = value; }
+        }
+        
+        private AccountDAO() { }
+
+        public bool compare(string password)
+        {
+            return password == Password;
+        }
+        public string getUsername()
+        {
+            return Username;
+        }
+        public bool Login(string userName, string Password)
+        {
+            string querry = "SELECT * FROM ACCOUNT WHERE userName = '" + userName + "' AND UserPass = '" + Password + "'";
+            DataTable data = dataProvider.Instance.excuteQuerry(querry);
+            return data.Rows.Count > 0;
+            this.Password = Password.Trim();
+            this.Username = userName.Trim();
+        }
+    }
+}
---
+++ WindowsFormsApp1/Other/AdminAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using WindowsFormsApp1.DAO;
+
+namespace WindowsFormsApp1
+{
+    public partial class AdminAccount : Form
+    {
+        public AdminAccount()
+        {
+            InitializeComponent();
+            load_FoodList();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void AdminAccount_Load(object sender, EventArgs e)
+        {
+
+            string querry = "SELECT UserName AS 'Tai Khoan', DisplayName AS 'Họ Tên', Type AS 'Chức Vụ' FROM ACCOUNT";
+            string querry2 = "EXEC PRO_GetUserByUserDisplayName @userDisplayName";
+            object[] p = new object[] { "admin" };
+            dtgvAccount.DataSource = dataProvider.Instance.excuteQuerry(querry2, p);
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            string querry = "SELECT * FROM ACCOUNT WHERE DisplayName like N'" + txtHoten.Text.ToLower() + "'";
+            dtgvAccount.DataSource = dataProvider.Instance.excuteQuerry(querry);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string querry = "SELECT name as 'Tên món ăn' FROM FOOD WHERE idFOODTYPE = " +
+                " (SELECT idFOODTYPE FROM FOODTYPE WHERE name = N'" + cbbFoodType.Text + "')";
+            DataTable pData = dataProvider.Instance.excuteQuerry(querry);
+            dtgvFood.DataSource = pData;
+            comboBox2.DataSource = pData;
+            comboBox2.DisplayMember = "Tên món ăn";
+            comboBox2.Text = cbbFoodType.Text;
+            dtgvFood.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+        private void load_FoodList()
+        {
+            string querry = "SELECT name FROM FOODTYPE";
+            DataTable pData = dataProvider.Instance.excuteQuerry(querry);
+            cbbFoodType.DataSource = pData;
+            cbbFoodType.DisplayMember = "name";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string querry = "INSERT dbo.FOODTYPE (name) VALUES (N" + textBox2.Text + ")";
+            dataProvider.Instance.excuteQuerry(querry);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string querry = "DELETE FROM dbo.FOODTYPE WHERE name = N'" + comboBox2.Text + "'";
+            dataProvider.Instance.excuteQuerry(querry);
+        }
+    }
+}
